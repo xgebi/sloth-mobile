d8446147eb7cebae58fbbb2bcc11647bba27a50d
Sarah Gebauer <xgebi@users.noreply.github.com>
2021-11-02 23:08:51 +0100

    Redoing this again, React world is not simple 😫

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import { AppProps } from 'next/app'
 import RootStore from '../store/RootStore'
 import { Provider } from "mobx-react";
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp(myAppArgs: any) {
+  const Component = myAppArgs.Component;
+  const pageProps = myAppArgs.pageProps
   const rootStore = new RootStore();
   return (
       <Provider
